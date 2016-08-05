7d0f7079449f3e4ddc50b4cb3f02496c7613d03f
Joshua Davey <dev+jgdavey@hashrocket.com>
2016-08-05 14:47:05 -0400

    Fixup pear sha, zsh formula

--- Formula/pear.rb
+++ Formula/pear.rb
@@ -3,12 +3,12 @@ require "formula"
 class Pear < Formula
   homepage "https://github.com/derekparker/go-pear"
   url "https://github.com/derekparker/go-pear/releases/download/v1.3.2/pear1.3.2.tar.gz"
-  sha1 "f4354f2f015b2e627754c7466830200744e82445"
+  sha256 "ee468483788f28cac5a64b029907d49b3b5da99748e32bde2a65e99d3eed1125"
 
   def install
     if ENV['SHELL'] == '/bin/zsh'
       ohai "Installing zshell auto complete function"
-      cp "_pear", File.expand_path("~/.zsh/functions")
+      cp "_pear", File.join(Etc.getpwuid.dir, ".zsh/functions")
     end
 
     ohai "Installing binary"
