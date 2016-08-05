8ffa8b1d6cb7c77f81a7bd17934ae3ac49583371
Joshua Davey <dev+jgdavey@hashrocket.com>
2016-08-05 14:44:33 -0400

    Fix unicornleap formula

--- Formula/unicornleap.rb
+++ Formula/unicornleap.rb
@@ -1,14 +1,23 @@
 require "formula"
+require "pathname"
+require "etc"
 
 class Unicornleap < Formula
   homepage "https://github.com/jgdavey/unicornleap"
-  url "https://github.com/jgdavey/unicornleap/archive/v0.1.0.tar.gz"
-  sha1 "4cc24cd63829fdca4ac6e8315ed72626e7971d4c"
+  url "https://github.com/jgdavey/unicornleap/archive/v1.0.2.tar.gz"
+  sha256 "153b297f9e4823e8ab227fa8725aa606d8bc59e2fa500d66d9ece9161c9de48d"
 
   def install
     system "make"
-    image_folder = File.join(File.expand_path("~"), ".unicornleap")
-    system "make", image_folder
+    system "make", "images"
+
+    fake_image_folder = Pathname(File.join(File.expand_path("~"), ".unicornleap"))
+    real_image_folder = Pathname(File.join(Etc.getpwuid.dir, ".unicornleap"))
+
+    if fake_image_folder.exist? && !real_image_folder.exist?
+      cp_r fake_image_folder, real_image_folder
+    end
+
     bin.install "build/unicornleap"
     man.mkpath
     man1.install "unicornleap.1"
