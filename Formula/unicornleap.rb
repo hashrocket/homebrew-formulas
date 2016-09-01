269a28fd0086debbbe2f35726ebec9deedbe95ca
Jon Allured <jon.allured@gmail.com>
2016-09-01 14:14:14 -0500

    Add description for unicornleap
    This is a required key now.

--- Formula/unicornleap.rb
+++ Formula/unicornleap.rb
@@ -2,6 +2,7 @@ require "pathname"
 require "etc"
 
 class Unicornleap < Formula
+  desc "Makes a unicorn fly across your screen. That's all it does."
   homepage "https://github.com/jgdavey/unicornleap"
   url "https://github.com/jgdavey/unicornleap/archive/v1.0.2.tar.gz"
   sha256 "153b297f9e4823e8ab227fa8725aa606d8bc59e2fa500d66d9ece9161c9de48d"
